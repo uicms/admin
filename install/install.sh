1f9d5b84c754f775d257a91130717bf0f7c619b8
Timothée Rolin <t.rolin@gmail.com>
2020-11-24 13:28:18 +0100

    Fix install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,5 +3,7 @@ if [ ! -d "public/themes" ]; then
     mkdir public/themes
 fi
 if [ ! -d "public/themes/admin" ]; then
-    ln -s vendor/uicms/admin/install/public/themes/admin public/themes/admin
+    cd public/themes
+    ln -s ../../vendor/uicms/admin/install/public/themes/admin public/themes/admin
+    cd ../..
 fi
