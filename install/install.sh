1a968434714fd43d0bb16c8eaa1ce90de12e2eac
Timothée Rolin <t.rolin@gmail.com>
2020-12-04 21:51:12 +0100

    Add link and new buttons in form links results

--- install/install.sh
+++ install/install.sh
@@ -4,6 +4,6 @@ if [ ! -d "public/themes" ]; then
 fi
 if [ ! -d "public/themes/admin" ]; then
     cd public/themes
-    ln -s ../../vendor/uicms/admin/install/public/themes/admin public/themes/admin
+    ln -s ../../vendor/uicms/admin/install/public/themes/admin admin
     cd ../..
 fi
