fcf6e43f906951f770c5d028e64cc0c1ae6339dc
Timothée Rolin <t.rolin@gmail.com>
2020-12-06 18:27:07 +0100

    Fix install.sh : change ln to cp theme

--- install/install (6 déc. 2020 à 18-26-30).sh
+++ install/install (6 déc. 2020 à 18-26-30).sh
@@ -4,6 +4,6 @@ if [ ! -d "public/themes" ]; then
 fi
 if [ ! -d "public/themes/admin" ]; then
     cd public/themes
-    cp -r ../../vendor/uicms/admin/install/public/themes/admin admin
+    ln -s ../../vendor/uicms/admin/install/public/themes/admin admin
     cd ../..
 fi
