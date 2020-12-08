cf0b857851eb445717dc59a5e4f19473765a56a0
Timothée Rolin <t.rolin@gmail.com>
2020-12-08 11:43:25 +0100

    Manage image size limit and display file informations in form

--- install/install (6 déc. 2020 à 18-26-30).sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -d "public/themes" ]; then
-    mkdir public/themes
-fi
-if [ ! -d "public/themes/admin" ]; then
-    cd public/themes
-    ln -s ../../vendor/uicms/admin/install/public/themes/admin admin
-    cd ../..
-fi
--- src/Form/DataTransformer/FileTransformer.php
+++ src/Form/DataTransformer/FileTransformer.php
@@ -10,9 +10,25 @@ use Uicms\App\Service\Uploader;
 
 class FileTransformer implements DataTransformerInterface
 {
+    protected $max_width = 1600;
+    protected $max_height = 1600;
+    protected $preview_max_width = 500;
+    protected $preview_max_height = 500;
+    protected $preview_prefix = '_';
+    protected $upload_folder = 'uploads';
+    protected $upload_path = 'public/uploads';
+    
     function __construct($field_config, $ui_config) {
         $this->field_config = $field_config;
         $this->ui_config = $ui_config;
+
+        if(isset($ui_config['upload_path'])) $this->upload_path = $ui_config['upload_path'];
+        if(isset($ui_config['upload_folder'])) $this->upload_folder = $ui_config['upload_folder'];
+        if(isset($ui_config['max_width'])) $this->max_width = $ui_config['max_width'];
+        if(isset($ui_config['max_height'])) $this->max_height = $ui_config['max_height'];
+        if(isset($ui_config['preview_max_width'])) $this->preview_max_width = $ui_config['preview_max_width'];
+        if(isset($ui_config['preview_max_height'])) $this->preview_max_height = $ui_config['preview_max_height'];
+        if(isset($ui_config['preview_prefix'])) $this->preview_prefix = $ui_config['preview_prefix'];
     }
     
     public function transform($string)
@@ -21,64 +37,71 @@ class FileTransformer implements DataTransformerInterface
             return null;
         }
         
-        $path = $this->ui_config['upload_path'] . '/' . $string;
-
+        # Check if file exists
+        $path = $this->upload_path . '/' . $string;
         if (!file_exists($path)) {
             return null;
         }
         
-        $upload_folder = $this->ui_config['upload_folder'];
-        $thumbnail_prefix = $this->ui_config['thumbnail_prefix'];
-        
+        # Create File object
         $file = new File($path);
-        $file->publicPath = '/' . trim($upload_folder, '/') . '/' . $string;
-        $file->thumbnailPath = '';
-        $thumbnail_path = '/' . trim($upload_folder, '/') . '/' . $thumbnail_prefix . $string;
-        if(file_exists($thumbnail_path)) {
-            $file->thumbnailPath = $thumbnail_path;
-        } 
-
+        $file->publicPath = '/' . trim($this->upload_folder, '/') . '/' . $string;
+        
+        # Add thumbnail path to object
+        $thumbnail_path = '/' . trim($this->upload_folder, '/') . '/' . $this->preview_prefix . $string;
+        $file->thumbnailPath = file_exists($thumbnail_path) ? $thumbnail_path : '';
+        
+        # Add dimensions
+        if($size = getImageSize($file->getRealPath())) {
+            $file->width = $size[0];
+            $file->height = $size[1];
+        }
+        
         return $file;
     }
 
     public function reverseTransform($file)
     {
         if ($file === null) return '';
-
         $mime_type = $file->getMimeType();
-        $uploader = new Uploader($this->ui_config['upload_path']);
-        $result = $uploader->upload($file);
-        $path = $this->ui_config['upload_path'];
-        $path_file = $path . '/' . $result;
-        $width_resize = isset($this->field_config['resize_width']) ? $this->field_config['resize_width'] : $this->ui_config['thumbnail_default_width'];
         
-        /* Make thumbnail */
+        # Upload
+        $uploader = new Uploader($this->upload_path);
+        $result = $uploader->upload($file);
+        $path_file = $this->upload_path . '/' . $result;
+        
+        /* Limit image width */
         if(strpos($mime_type, 'image') === 0) {
-            $path_thumbnail = $path . '/' . $this->ui_config['thumbnail_prefix'] . $result;
             $img = Image::make($path_file);
-            $img->resize($width_resize, null, function($constraint){
+            $img->resize($this->max_width, null, function($constraint){
                 $constraint->aspectRatio();
                 $constraint->upsize();
             });
-            $img->save($path_thumbnail);
+            $img->save($this->upload_path . '/' . $result);
         }
         
+        /* Make image thumbnail */
+        if(strpos($mime_type, 'image') === 0) {
+            $img = Image::make($path_file);
+            $img->resize($this->preview_max_width, null, function($constraint){
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            });
+            $img->save($this->upload_path . '/' . $this->preview_prefix . $result);
+        }
+        
+        /* Make video thumbnail */
         if(strpos($mime_type, 'video') === 0) {
-            # Calculate half time
     		$cmd_duration = "ffmpeg -i \"" . addslashes($path_file) . "\" 2>&1 | grep Duration | awk '{print $2}' | tr -d ,";
     		if($duration = exec($cmd_duration)) {
-        		preg_match("'([0-9]{2}):([0-9]{2}):([0-9]{2})'", $duration, $preg);
+        		# Calculate half time
+                preg_match("'([0-9]{2}):([0-9]{2}):([0-9]{2})'", $duration, $preg);
         		$seconds = (($preg[1]*3600) + ($preg[2]*60) + $preg[3])/2;
         		$half = sprintf('%02d:%02d:%02d', ($seconds/ 3600),($seconds/ 60 % 60), $seconds% 60);
-            
-                # Filter
-                $filter = "-filter:v scale=\"" . $width_resize . ":-1\"";
-            
+
                 # Generate thumbnail
                 $path_parts = pathinfo($path_file);
-                $path_thumbnail = $path . '/' . $this->ui_config['thumbnail_prefix'] . $path_parts['filename']. '.jpg';
-        		$cmd_thumbnail = "ffmpeg -i \"". addslashes($path_file) . "\" -an -ss $half -r 1 -vframes 1 -y $filter \"" . $path_thumbnail . "\"";
-        		exec($cmd_thumbnail);
+        		exec("ffmpeg -i \"". addslashes($path_file) . "\" -an -ss $half -r 1 -vframes 1 -y -filter:v scale=\"" . $this->preview_max_width . ":-1\" \"" . $this->upload_path . '/' . $this->preview_prefix . $path_parts['filename']. '.jpg' . "\"");
     		} else {
     		    throw new Exception('Video not readable or in wrong format!');
     		}
