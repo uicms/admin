bb12f8c9b91800f09312eab83bab08dda79a0702
Timothée Rolin <t.rolin@gmail.com>
2021-02-07 18:32:10 +0100

    Add --delete to rsync command in update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ -d "public/themes/admin" ]; then
-    rsync -avz vendor/uicms/admin/install/public/themes/admin/ public/themes/admin/
+    rsync -avz --delete vendor/uicms/admin/install/public/themes/admin/ public/themes/admin/
 fi
