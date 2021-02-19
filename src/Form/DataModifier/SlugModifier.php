dc7de5475dddf95e3eed6aacb1b9b8f2d7a16ef0
Timothée Rolin <t.rolin@gmail.com>
2021-02-19 11:43:26 +0100

    Allow in a form to set slug manually

--- src/Form/DataModifier/SlugModifier.php
+++ src/Form/DataModifier/SlugModifier.php
@@ -14,9 +14,14 @@ class SlugModifier
     public function modify($value)
     {
         $slugger = new AsciiSlugger();
-        $get_method = 'get' . str_replace('_', '', ucwords($this->table_config['name_field'], '_'));
-        $slug = strtolower($slugger->slug($this->data->$get_method()));
-         
-        return $slug;
+        
+        # Set slug only if value is empty or equal to previous slug
+        if(!trim($value) || ($value == strtolower($slugger->slug($this->data->_name)))) {
+            $get_method = 'get' . str_replace('_', '', ucwords($this->table_config['name_field'], '_'));
+            $slug = strtolower($slugger->slug($this->data->$get_method()));
+            return $slug;
+        } else {
+            return $value;
+        }
     }
 }
