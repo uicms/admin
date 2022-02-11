f742f914e017f4926198dbdb5e7da0e2626b57f3
Timothée Rolin <t.rolin@gmail.com>
2022-02-11 15:51:27 +0100

    Add excluded option to forms

--- src/Form/Type/UIFormType.php
+++ src/Form/Type/UIFormType.php
@@ -36,52 +36,59 @@ class UIFormType extends AbstractType
         # Translations
         if(isset($form_config['translations']) && $form_config['translations']) {
             $translations_fields = $form_config['translations'];
-            $options = array();
+            $displayed_fields = [];
+            $excluded_fields = [];
+            
             foreach($translations_fields as $field_name=>$field_config) {
-                $translations_fields[$field_name]['options']['field_type'] = $field_config['namespace'] . '\\' . $field_config['type'];
-                $options[$field_name] = $translations_fields[$field_name]['options'];
-                
-                # Translate label
-                if(isset($translator) && isset($options[$field_name]['label']) && $options[$field_name]['label']) {
-                    $options[$field_name]['label'] = $translator->trans($options[$field_name]['label'], [], 'admin');
+                if(isset($field_config['excluded']) && $field_config['excluded']) {
+                    $excluded_fields[] = $field_name;
+                } else {
+                    $translations_fields[$field_name]['options']['field_type'] = $field_config['namespace'] . '\\' . $field_config['type'];
+                    $displayed_fields[$field_name] = $translations_fields[$field_name]['options'];
+
+                    # Translate label
+                    if(isset($translator) && isset($displayed_fields[$field_name]['label']) && $displayed_fields[$field_name]['label']) {
+                        $options[$field_name]['label'] = $translator->trans($displayed_fields[$field_name]['label'], [], 'admin');
+                    }
                 }
             }
             
-            $builder->add('translations', TranslationsType::class, array('fields'=>$options));
+            $builder->add('translations', TranslationsType::class, array('excluded_fields'=>$excluded_fields, 'fields'=>$displayed_fields));
             $builder->get('translations')->addModelTransformer(new TranslationsTransformer($form_config, $ui_config));
         }
         
         # Fields
         foreach($form_config['fields'] as $field_config) {
+            if(!isset($field_config['excluded']) || !$field_config['excluded']) {
+                # Entity Type : order by main field asc
+                if($field_config['type'] == 'EntityType') {
+                    $field_config['options']['query_builder'] = function($model) {
+                        $query = $model->createQueryBuilder('t');
+                        $name_field = $model->getConfig('name_field');
+                        if($model->isTranslatable() && $model->isFieldTranslatable($name_field)) {
+                            $query->join('t.translations', 'i');
+                            $query->where("i.locale = '" . $this->params->get('locale') . "'");
+                            $query->orderBy("i.$name_field", 'ASC');
+                        } else {
+                            $query->orderBy("t.$name_field", 'ASC');
+                        }
+                        return $query;
+                    };
+                }
             
-            # Entity Type : order by main field asc
-            if($field_config['type'] == 'EntityType') {
-                $field_config['options']['query_builder'] = function($model) {
-                    $query = $model->createQueryBuilder('t');
-                    $name_field = $model->getConfig('name_field');
-                    if($model->isTranslatable() && $model->isFieldTranslatable($name_field)) {
-                        $query->join('t.translations', 'i');
-                        $query->where("i.locale = '" . $this->params->get('locale') . "'");
-                        $query->orderBy("i.$name_field", 'ASC');
-                    } else {
-                        $query->orderBy("t.$name_field", 'ASC');
-                    }
-                    return $query;
-                };
-            }
+                # Translate label
+                if(isset($translator) && isset($field_config['options']['label']) && $field_config['options']['label']) {
+                    $field_config['options']['label'] = $translator->trans($field_config['options']['label'], [], 'admin');
+                }
             
-            # Translate label
-            if(isset($translator) && isset($field_config['options']['label']) && $field_config['options']['label']) {
-                $field_config['options']['label'] = $translator->trans($field_config['options']['label'], [], 'admin');
-            }
+                # Add field to form
+                $builder->add($field_config['name'], $field_config['namespace'] . '\\' . $field_config['type'], $field_config['options']);
             
-            # Add field to form
-            $builder->add($field_config['name'], $field_config['namespace'] . '\\' . $field_config['type'], $field_config['options']);
-            
-            # Add transformer to field
-            if(isset($field_config['transformer']) && $field_config['transformer']) {
-                eval("\$transformer = new " . $field_config['transformer'] . "(\$field_config, \$ui_config);");
-                $builder->get($field_config['name'])->addModelTransformer($transformer);
+                # Add transformer to field
+                if(isset($field_config['transformer']) && $field_config['transformer']) {
+                    eval("\$transformer = new " . $field_config['transformer'] . "(\$field_config, \$ui_config);");
+                    $builder->get($field_config['name'])->addModelTransformer($transformer);
+                }
             }
         }
          
