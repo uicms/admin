3a36c6dd64cd9e37a039e99c286d3ba1aeef693e
Timothée Rolin <t.rolin@gmail.com>
2023-03-22 16:11:16 +0100

    Modify FileTransformer (do not use Upload service anymore)

--- src/Form/DataTransformer/FileTransformer.php
+++ src/Form/DataTransformer/FileTransformer.php
@@ -4,6 +4,8 @@ namespace Uicms\Admin\Form\DataTransformer;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 use Intervention\Image\ImageManagerStatic as Image;
 use Uicms\App\Service\Uploader;
@@ -63,55 +65,79 @@ class FileTransformer implements DataTransformerInterface
         return $file;
     }
     
-    public function reverseTransform($file)
+    public function reverseTransform($file, $slug=true, $make_unique=true)
     {
         if ($file === null) return '';
         $mime_type = $file->getMimeType();
 
         # Upload
-        $uploader = new Uploader($this->upload_path);
-        $file_name = $uploader->upload($file);
-        $path_file = $this->upload_path . '/' . $file_name;
+        if($file instanceof UploadedFile) {
+            $file_name = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $extension = $file->getClientOriginalExtension();
+        } else if ($file instanceof File) {
+            $file_name = pathinfo($file->getFileName(), PATHINFO_FILENAME);
+            $extension = $file->getExtension();
+        } else {
+            throw new \Exception('file_error');
+        }
+        $file_source = $file->getPathName();
+
+        #dd(get_class_methods($file));
+        #dd($file_name);
+        
+        /* File name */
+        if($slug) {
+            $slugger = new AsciiSlugger();
+            $file_name = strtolower($slugger->slug($file_name));
+        }
+        if($make_unique) {
+            $file_name = $file_name . '-' . uniqid();
+        }
+
+        /* Final path */
+        $file_dest = $this->upload_path . '/' . $file_name . '.' . $extension;
+        rename($file_source, $file_dest);
+        chmod($file_dest, 0755);
         
         /* Limit image width */
         if(strpos($mime_type, 'image') === 0 && strpos($mime_type, 'svg') === false) {
-            $img = Image::make($path_file);
+            $img = Image::make($file_dest);
             $img->resize($this->max_width, $this->max_height, function($constraint){
                 $constraint->aspectRatio();
                 $constraint->upsize();
             });
-            $img->save($this->upload_path . '/' . $file_name);
+            $img->save($file_dest);
         }
         
         /* Make image thumbnail */
         if(strpos($mime_type, 'image') === 0 && strpos($mime_type, 'svg') === false) {
-            $img = Image::make($path_file);
+            $img = Image::make($file_dest);
             $img->resize($this->preview_max_width, $this->preview_max_height, function($constraint){
                 $constraint->aspectRatio();
                 $constraint->upsize();
             });
-            $img->save($this->upload_path . '/' . $this->preview_prefix . $file_name);
+            $img->save($this->upload_path . '/' . $this->preview_prefix . $file_name . '.' . $extension);
         } else if(strpos($mime_type, 'svg')) {
-            copy($path_file, $this->upload_path . '/' . $this->preview_prefix . $file_name);
+            copy($file_dest, $this->upload_path . '/' . $this->preview_prefix . $file_name . '.' . $extension);
         }
         
         /* Make video thumbnail */
         if($this->video_generate_thumbnail && strpos($mime_type, 'video') === 0) {
-    		$cmd_duration = "ffmpeg -i \"" . addslashes($path_file) . "\" 2>&1 | grep Duration | awk '{print $2}' | tr -d ,";
-    		if($duration = exec($cmd_duration)) {
-        		# Calculate half time
+            $cmd_duration = "ffmpeg -i \"" . addslashes($file_dest) . "\" 2>&1 | grep Duration | awk '{print $2}' | tr -d ,";
+            if($duration = exec($cmd_duration)) {
+                # Calculate half time
                 preg_match("'([0-9]{2}):([0-9]{2}):([0-9]{2})'", $duration, $preg);
-        		$seconds = (($preg[1]*3600) + ($preg[2]*60) + $preg[3])/2;
-        		$half = sprintf('%02d:%02d:%02d', ($seconds/ 3600),($seconds/ 60 % 60), $seconds% 60);
+                $seconds = (($preg[1]*3600) + ($preg[2]*60) + $preg[3])/2;
+                $half = sprintf('%02d:%02d:%02d', ($seconds/ 3600),($seconds/ 60 % 60), $seconds% 60);
 
                 # Generate thumbnail
-                $path_parts = pathinfo($path_file);
-        		exec("ffmpeg -i \"". addslashes($path_file) . "\" -an -ss $half -r 1 -vframes 1 -y -filter:v scale=\"" . $this->preview_max_width . ":-1\" \"" . $this->upload_path . '/' . $this->preview_prefix . $path_parts['filename']. '.jpg' . "\"");
-    		} else {
-    		    throw new \Exception('Video not readable or in wrong format or FFMpeg is not installed!');
-    		}
+                $path_parts = pathinfo($file_dest);
+                exec("ffmpeg -i \"". addslashes($file_dest) . "\" -an -ss $half -r 1 -vframes 1 -y -filter:v scale=\"" . $this->preview_max_width . ":-1\" \"" . $this->upload_path . '/' . $this->preview_prefix . $path_parts['filename']. '.jpg' . "\"");
+            } else {
+                throw new \Exception('Video not readable or in wrong format or FFMpeg is not installed!');
+            }
         }
         
-        return $file_name;
+        return $file_name . '.' . $extension;
     }
 }
